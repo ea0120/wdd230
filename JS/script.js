5c2df310bcc8f521f6e06388a5be6635c3092a67
ea0120 <97117717+ea0120@users.noreply.github.com>
2022-02-03 20:42:47 -0600

    Delete JS directory

--- JS/script.js
+++
@@ -1,5 +0,0 @@
-window.onload = function getTime() {
-    let currentDate = new Date();
-    document.querySelector(".year").innerHTML = currentDate.getFullYear();
-    document.getElementById("lastmod").innerHTML = document.lastModified;
-}
