ada69f7b5b0f5c9c18bc0f4f0dc76317c1d34714
ea0120 <97117717+ea0120@users.noreply.github.com>
2022-02-13 18:42:08 -0600

    fix formula

--- chamber/js/windChill.js
+++ chamber/js/windChill.js
@@ -1,5 +1,5 @@
 var t = 33;
 var s = 3;
-var f = 35.74 + 0.6215t - 35.75 s^0.16 + 0.4275t s^0.16;
+var f = 35.74 + 0.6215t - 35.75s^0.16 + 0.4275ts^0.16;
 var windChillText = document.getElementById("windchill");
 winndChillText.innerHtml = "Wind Chill:&emsp;&emsp;&emsp;&emsp; " + f;
