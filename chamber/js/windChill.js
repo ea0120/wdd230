4180b847b847f38b4e78f4b91dfcd2b5a3070d0a
ea0120 <97117717+ea0120@users.noreply.github.com>
2022-02-13 18:38:23 -0600

    wind chill update.

--- chamber/js/windChill.js
+++ chamber/js/windChill.js
@@ -1,3 +1,5 @@
-var f = 35.74 + 0.6215t - 35.75 s^0.16 + 0.4275 t s^0.16;
+var t = 33;
+var s = 3;
+var f = 35.74 + 0.6215t - 35.75 s^0.16 + 0.4275t s^0.16;
 var windChillText = document.getElementById("windchill");
-winndChillText.innerHtml = f;
+winndChillText.innerHtml = "Wind Chill:&emsp;&emsp;&emsp;&emsp; " + f;
