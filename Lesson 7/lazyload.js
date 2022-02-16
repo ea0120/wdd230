fe215916fa7a27c793ab7e6d24180d5b86f58bdc
ea0120 <amos21005@byu1.edu>
2022-02-16 16:53:37 -0500

    added lesson 7

---
+++ Lesson 7/lazyload.js
@@ -0,0 +1,32 @@
+const images = document.querySelectorAll("[data-scr]")
+
+function preloudImage(img){
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src = src;
+}
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -500px 0px"
+};
+
+const imgObserver = new IntersectionObserver((enteries, imgObserver) => {
+    enteries.forEach(entry => {
+        if (!entry/inInerselecting) {
+            return;
+        } else{
+            preloudImage(entry.target)
+            imgObserver.unobserve(entry.target)
+        }
+    })
+
+}, imgOptions);
+
+images.forEach(images => {
+    imgObserver.observe(image);
+}
+
+);
