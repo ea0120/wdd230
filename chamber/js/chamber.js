320658bb14651c12cf638635f9337552e3fb553b
ea0120 <97117717+ea0120@users.noreply.github.com>
2022-02-19 19:13:56 -0600

    Updated Discovery Page

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -6,3 +6,27 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 
 datefield.innerHTML = fulldate;
+
+
+if (typeof Storage !== "undefined") {
+    if (localStorage.visitcount) {
+      document.getElementById("result").innerHTML =
+        "Welcome back, you've been here " +
+        localStorage.visitcount +
+        " times before.";
+      localStorage.visitcount = Number(localStorage.visitcount) + 1;
+    } else {
+      localStorage.visitcount = 1;
+      document.getElementById("result").innerHTML =
+        "This is your first time here! Welcome.";
+    }
+    // document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+  } else {
+     alert("Sorry, your browser does not support web storage.  Changes will not be saved");
+    document.getElementById("result").innerHTML =
+      "Sorry, your browser does not support web storage...";
+  }
+  
+  console.log("localstorage visit count now: " + localStorage.visitcount);
+  
+  // localStorage.clear();
