25826a0018df8037e7ec8447634efa839cf2b2a9
ea0120 <97117717+ea0120@users.noreply.github.com>
2022-03-02 19:54:23 -0600

    Remove

--- lesson2/scripts/main.js
+++
@@ -1,5 +0,0 @@
-const year = document.getElementById("year");
-const lastupdate = document.getElementById("lastupdate");
-
-year.textContent = new Date().getFullYear();
-lastupdate.textContent = document.lastModified;
