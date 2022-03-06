a09cc14f6dcdf196687771aa1cf7fe06dcfdf36e
ea0120 <97117717+ea0120@users.noreply.github.com>
2022-03-05 19:10:42 -0600

    Update table build function

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,9 +1,13 @@
 const requestURL = 'https://ea0120.github.io/wdd230/chamber/json/data.json';
-const cards = document.querySelector('.cards');
-
-//Count Prophets
-let count = 1;
-
+const table = document.querySelector('table');
+let headrow = document.createElement('tr');
+table.appendChild(headrow);
+let header = ["Name", "Address", "Website"];
+header.forEach(function(item, index) {
+    let tablehead = document.createElement('th');
+    tablehead.textContent = item;
+    headrow.appendChild(tablehead);
+});
 
 //Fetch Data from JSON
 fetch(requestURL)
@@ -16,33 +20,12 @@ fetch(requestURL)
   })
 
 function displayBusiness(businessDirectory) {
-  // Create elements to add to the document
-  let card = document.createElement('section');
-  let h2 = document.createElement('h2');
-  let adr = document.createElement('h4');
-  let web = document.createElement('h4');
-  let newRow = document.createElement('table')
-  
-  //Name of business
-  h2.textContent = `${businessDirectory.name}`;
-
-  //DOB
-  adr.textContent = `Address: ${businessDirectory.address}`
-
-  //POB
-  web.textContent = `Website: ${businessDirectory.website}`
-
-  //newRow
-  newRow.textContent = `${businessDirectory}`
-
-  //Add children
-  card.appendChild(h2);
-  card.appendChild(adr);
-  card.appendChild(web);
-
-  //Add Prophet Card
-  cards.appendChild(card);
-  
-  //Count Prophets
-  count++;
+    let tr = document.createElement('tr');
+    let items = [`${businessDirectory.name}`,`${businessDirectory.address}`,`${businessDirectory.website}`];
+    items.forEach(function(item, index) {
+        let td = document.createElement('td');
+        td.textContent = item;
+        tr.appendChild(td);
+    });
+    table.appendChild(tr);
 }
