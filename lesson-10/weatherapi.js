2ba5f57de4011053eb23cbd13ff59d909f23092d
ea0120 <amos21005@byu1.edu>
2022-03-09 15:06:00 -0500

    added to json file

--- lesson-10/weatherapi.js
+++ lesson-10/weatherapi.js
@@ -4,3 +4,10 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
   });
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
