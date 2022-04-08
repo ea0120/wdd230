9b5469e723bb08d701a68a38ec36faa20bd8ca64
ea0120 <97117717+ea0120@users.noreply.github.com>
2022-04-07 20:25:26 -0500

    Just Elememt

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,5 +1,5 @@
 // select the elements to manipulate (output to)
-const datefield = document.getElementsById("date");
+const datefield = document.getElementById("date");
 
 // derive the current date using a date object
 const now = new Date();
